e0c45428e23435123bd9f26809af731c2548e4bb
bleuf1sh <2596120+bleuf1sh@users.noreply.github.com>
2019-03-15 10:44:16 -0500

    reduced the sdk verbosity

--- setupLabsMac.sh
+++ setupLabsMac.sh
@@ -306,16 +306,17 @@ function installDevLanguagesAndIDEs() {
   fi
   refreshBash
 
-  set -x
   local sdkman_java_version=sdk list java | tr " " "\n" | grep -o "^11.*open" | head -1
+  echo "Installing java $sdkman_java_version"
   sdk install java "$sdkman_java_version"
 
   local sdkman_gradle_version=sdk list gradle | tr " " "\n" | grep -o "^5.2.*" | head -1
+  echo "Installing gradle $sdkman_gradle_version"
   sdk install gradle "$sdkman_gradle_version"
 
   local sdkman_springboot_version=sdk list springboot | tr " " "\n" | grep -o "^2.1.*" | head -1
+  echo "Installing SpringBoot $sdkman_springboot_version"
   sdk install springboot "$sdkman_springboot_version"
-  set +x
 
   echo 'Installing IntelliJ Suite...'
   brew cask install jetbrains-toolbox --force #guard against pre-installed jetbrains-toolbox
