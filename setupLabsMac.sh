753f007212f19099e0872fcd0e16f94ce598b763
bleuf1sh <2596120+bleuf1sh@users.noreply.github.com>
2019-03-15 10:40:55 -0500

    fixed silly sdk man install issue

--- setupLabsMac.sh
+++ setupLabsMac.sh
@@ -308,13 +308,13 @@ function installDevLanguagesAndIDEs() {
 
   set -x
   local sdkman_java_version=sdk list java | tr " " "\n" | grep -o "^11.*open" | head -1
-  sdk install "$sdkman_java_version"
+  sdk install java "$sdkman_java_version"
 
   local sdkman_gradle_version=sdk list gradle | tr " " "\n" | grep -o "^5.2.*" | head -1
-  sdk install "$sdkman_gradle_version"
+  sdk install gradle "$sdkman_gradle_version"
 
   local sdkman_springboot_version=sdk list springboot | tr " " "\n" | grep -o "^2.1.*" | head -1
-  sdk install "$sdkman_springboot_version"
+  sdk install springboot "$sdkman_springboot_version"
   set +x
 
   echo 'Installing IntelliJ Suite...'
