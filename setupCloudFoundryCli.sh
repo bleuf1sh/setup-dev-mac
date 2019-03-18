4a27e99b2b328ddd783036cfc28e8ebf8fa78639
bleuf1sh <2596120+bleuf1sh@users.noreply.github.com>
2019-03-18 10:21:11 -0500

    attempting to get the modules to not be cyclic

--- setupCloudFoundryCli.sh
+++ setupCloudFoundryCli.sh
@@ -1,7 +1,9 @@
-source setupBase.sh
+echo "in here"
+source setupBase.sh loadFuncs
 
 echo
 echo "Installing Cloud Foundry CLI..."
+
 brew tap cloudfoundry/tap
 brew install cf-cli --force #guard against pre-installed version
 
