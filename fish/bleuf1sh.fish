2788c857a2057c93494060d4febbde594393f639
Aaron <2596120+bleuf1sh@users.noreply.github.com>
2019-05-07 10:05:35 -0500

    Added git aliases to fish

--- fish/bleuf1sh.fish
+++ fish/bleuf1sh.fish
@@ -5,3 +5,27 @@ end
 function x --description 'alias x=exit'
 	exit  $argv;
 end
+
+function s --description 'alias s=git status'
+	git status;
+end
+
+function gs --description 'alias gs=git status'
+	git status;
+end
+
+function gst --description 'alias gst=git status'
+	git status;
+end
+
+function ga --description 'alias ga=git add'
+	git add $argv;
+end
+
+function co --description 'alias co=git checkout'
+	git checkout $argv;
+end
+
+function go --description 'alias go=git checkout'
+	git checkout $argv;
+end
